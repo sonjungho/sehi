3017241c32cd934ce5f93263b5492434e8dc7043
son jungho <40311263+sonjungho@users.noreply.github.com>
2018-06-24 15:34:02 +0900

    Add files via upload

---
+++ down.php
@@ -0,0 +1,98 @@
+﻿<html>
+<head>
+<script language="javascript">
+function click_OK(){
+  f1.submit();
+}
+
+function search_click(){
+  var sch_string;
+  sch_string=f1.q.value;
+  top.main1.location.href="search/perlfect/search/search.pl?q="+sch_string;
+}
+</SCRIPT>
+<title>프로그램</title>
+</head>
+<body bgcolor="#ffffff" text="#00FFFF" link="#2494C4" vlink="#800080" alink="#FF0000">
+
+<table border="4" cellspacing="1" bgcolor="#808080" bordercolordark="#808080" height="357" width="160">
+
+<tr><td height="24" bgcolor="blue" valign="middle" bordercolor="#FFFFCC" width="160">
+<center><b><font face="굴림" size="3" color="#FFFFFF">게시판 안내</font></b></center>
+</td></tr>
+
+
+<tr><td height="25" bgcolor="#FFFFCC" valign="middle" bordercolor="#FFFFCC" width="160">
+&nbsp;<a href="/karts/bbs/zboard.php?id=board106" target="main1">
+<font face="굴림체" size="3" color="#000000"><b>부채춤 작품</b></font></a>
+</td></tr>
+
+<tr><td height="25" bgcolor="#FFFFCC" valign="middle" bordercolor="#FFFFCC" width="160">
+&nbsp;<a href="/karts/bbs/zboard.php?id=board108" target="main1">
+<font face="굴림체" size="3" color="#000000"><b>꼭두각시 작품</b></font></a>
+</td></tr>
+
+<tr><td height="25" bgcolor="#FFFFCC" valign="middle" bordercolor="#FFFFCC" width="160">
+&nbsp;<a href="/karts/bbs/zboard.php?id=board104" target="main1">
+<font face="굴림체" size="3" color="#000000"><b>소고/탈춤 작품</b></font></a>
+</td></tr>
+
+<tr><td height="25" bgcolor="#FFFFCC" valign="middle" bordercolor="#FFFFCC" width="160">
+&nbsp;<a href="/karts/bbs/zboard.php?id=board107" target="main1">
+<font face="굴림체" size="3" color="#000000"><b>기타 무용작품</b></font></a>
+</td></tr>
+
+
+<tr><td height="25" bgcolor="yellow" valign="middle" width="160"><center>
+<a href="/karts/bbs/zboard.php?id=board109" target="main1">
+<font face="굴림체" size="3" color="blue"><b>음악 다운로드</b></font></a></center>
+</td></tr>
+
+<tr><td height="25" bgcolor="yellow" valign="middle" width="160"><center>
+<a href="/karts/bbs/zboard.php?id=board114" target="main1">
+<font face="굴림체" size="3" color="blue"><b>소품과 의상</b></font></a></center>
+</td></tr>
+
+<tr><td height="25" bgcolor="#FFFFCC" valign="middle" width="160"><center>
+<a href="http://facebook.com/dancekorea" target="new">
+<font face="굴림체" size="3" color="blue"><b>페북</b></font></a>
+<a href="https://www.instagram.com/sonjeonghomikesohn/" target="new">/
+<font face="굴림체" size="3" color="blue"><b>인스타</b></font></a>
+</center>
+</td></tr>
+
+<tr><td height="25" bgcolor="#FFFFCC" valign="middle" width="160">
+&nbsp;<a href="http://blog.naver.com/coldhunter" target="new">
+<font face="굴림체" size="3" color="blue"><b>세희만물 블로그</b></font></a>
+</td></tr>
+
+<tr><td height="25" bgcolor="#FFFFCC" valign="middle" width="160">
+<font color="black"><b>카페:</b><font><a href="http://cafe.naver.com/01083380380" target="new">
+<font face="굴림체" size="3" color="blue"><b>네이버</b></font></a>
+<a href="http://cafe.daum.net/01083380380" target="new">/
+<font face="굴림체" size="3" color="blue"><b>다음</b></font></a>
+</td></tr>
+
+
+</table>
+<br>
+<table border="4" cellspacing="1" bgcolor="#C0C0C0" bordercolordark="#808080" width="160" height="25" bordercolorlight="#C0C0C0" bordercolor="#C0C0C0">
+
+<tr>
+<td width="160" height="37" bgcolor="#000000" valign="middle">
+<font color="#ffffff" size="3"><b>
+&nbsp;입금계좌:<br>
+&nbsp;농협 (백혜영)<br>
+&nbsp;064-02-311726<hr>
+&nbsp;담당자: 손 정호<br>
+&nbsp;카톡: dancekorea<br>
+<font size=2>&nbsp;jst4567@gmail.com</font><br>
+&nbsp;010-8338-0380<br>
+&nbsp;070-8900-1188
+
+</b> 
+</font>
+</td></tr>
+</table>
+
+</body></html>
